525ea01b896ef8b603b418cda290197ce846cd82
Jake Mareno <jmareno@unc.edu>
2024-02-21 18:33:01 -0500

    finished?

--- script.js
+++ script.js
@@ -30,16 +30,15 @@ function createSnackPellets() {
   let path = null; 
   
   for ( let y_pellet = space; y_pellet < canvas.height; y_pellet+= space ) {
-  
-    for ( let x_pellet = space; x_pellet < canvas.width; x_pellet+= space ) {
     
-      if ( hypotenus( ( x_pellet - canvas.width/2 ), ( y_pellet - canvas.height/2 ) )  >  ( radius + space/2 ) ) {
+    for ( let x_pellet = space; x_pellet < canvas.width; x_pellet+= space ) {
       
+      if ( hypotenus( ( x_pellet - canvas.width/2 ), ( y_pellet - canvas.height/2 ) )  >  ( radius + space/2 ) ) {
         path = new Path2D();
         path.arc( x_pellet, y_pellet, pellet_radius, 0, 2*Math.PI );
         snack_pellets[index++] = { x: x_pellet, y: y_pellet, circle:path };
       }
-     
+      
     }
     
   }
@@ -76,21 +75,21 @@ function createModel() {
 
 
 function startGame() {
-    // Center pacman
-    x_pacman = 150;
-    y_pacman = 75;
+  // Center pacman
+  x_pacman = 150;
+  y_pacman = 75;
+  
+  time_index = 0;
+  score = 0;
+  
+  pacman_model = [];
+  snack_pellets = [];
 
-    time_index = 0;
-    score = 0;
-
-    key = "ArrowRight";
-    pause = true;
-
-    pacman_model = [];
-    snack_pellets = [];
-
-    createModel();
-    createSnackPellets();
+  key = "ArrowRight";
+  paused = true;
+  
+  createModel();
+  createSnackPellets();
 }
 
 
@@ -100,57 +99,99 @@ function startGame() {
 document.addEventListener( "keyup", keyEvent );
 
 function keyEvent( event ) {
-  if (event.key === ' ') {
-    paused = !paused;
-  } else if (event.key = 's') {
-    startGame();
-  } 
-
-  if (!paused) {
-    key = event.key;
-  }
   
+  if (event.key == "s") {
+    startGame();
+    console.log("starting game");
+  } else if (event.key == " ") {
+    paused = !paused;
+    console.log("paused = " + paused);
+  } else if (!paused) {
+    key = event.key;
+    console.log("key = " + key);
+  }
+
 }
-
-
-
-
 
 
 // ----------------------------------------------
 // Task 3: Put JS code below.
 // ----------------------------------------------
 
-
 function draw() {
-    context2d.clearRect(0, 0, 150, 300);
-    context2d.save();
+  document.getElementById("score").innerText = score;
 
-    context2d.fillStyle = "black";
-    context2d.strokeStyle = "black";
+  context2d.clearRect(0, 0, 300, 150);
 
-    score = 0;
-    for(let i = 0; i < snack_pellets.length; i++)  {
-        let pellet = snack_pellets[i];
+  context2d.fillStyle = "black";
+  context2d.strokeStyle = "black";
 
-
-        if( hypotenus((x_pacman - pellet.x), (y_pacman - pellet.y)) < (radius/2) ) {
-            snack_pellets.splice(i, 1);
-            score += 10;
-        }
-   }
-
-    document.getElementById("score").innerText = score;
+  context2d.save();
+  
+  for(let i = 0; i < snack_pellets.length; i++)  {
+    let pellet = snack_pellets[i];
     
-    
-    for(let pellet of snack_pellets) {
-        context2d.fill(pellet.circle);
-        context2d.stroke(pellet.circle);
+    if( hypotenus((x_pacman - pellet.x), (y_pacman - pellet.y)) < (radius/2) ) {
+      snack_pellets.splice(i, 1);
+      score += 10;
     }
+  }
+  
+  for(let pellet of snack_pellets) {
+    context2d.fill(pellet.circle);
+    context2d.stroke(pellet.circle);
+  }
+
+  if(x_pacman <= radius) {
+    x_pacman = radius;
+  } else if(x_pacman >= (300 - radius)) {
+    x_pacman = 300 - radius;
+  }
+
+  if(y_pacman <= radius) {
+    y_pacman = radius;
+  } else if(y_pacman >= (150 - radius)) {
+    y_pacman = 150 - radius;
+  }
+
+  context2d.translate(x_pacman, y_pacman);
+
+  switch(key) {
+  	case "ArrowUp":
+    	context2d.rotate(Math.PI*3/2);
+      break;
+    case "ArrowDown":
+    	context2d.rotate(Math.PI/2);
+      break;
+    case "ArrowLeft":
+    	context2d.rotate(Math.PI);
+      break;
+  }
+
+  if(!paused) {
+    switch(key) {
+      case "ArrowUp":
+        y_pacman -= displacement;
+        break;
+      case "ArrowDown":
+        y_pacman += displacement;
+        break;
+      case "ArrowLeft":
+        x_pacman -= displacement;
+        break;
+      case "ArrowRight":
+        x_pacman += displacement;
+        break;
+    }
+  }
+
+  context2d.fillStyle = "yellow";
+  context2d.fill(pacman_model[time_index]);
+  context2d.stroke(pacman_model[time_index]);
+  context2d.restore();
+
+  time_index = ++time_index % 4;
 }
-
-
-
 
 
 // ----------------------------------------------
